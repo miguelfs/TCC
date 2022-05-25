193f14183838d31bdacb052b003919a748fa5424
Miguel de Sousa <mfsousa@poli.ufrj.br>
2022-05-25 11:02:33 -0300

    add tests

--- modulation_toolbox_py/filterbank/designfilterbank_stft_test.py
+++ modulation_toolbox_py/filterbank/designfilterbank_stft_test.py
@@ -1,9 +1,29 @@
 import unittest
-from designfilterbank_stft import windowBandwidth, designfilterbankstft, downsample
+from designfilterbank_stft import windowBandwidth, designfilterbankstft, downsample, upsample, STFTwindow, \
+    generate_analysis_win
 import numpy as np
 
 
 class TestDesignFilterBankSTFT(unittest.TestCase):
+    def test_designfilterbankstft(self):
+        numHalfBands = 64
+        sharpness = 9
+        decFactor = 64 // 4
+        fb1 = designfilterbankstft(numHalfBands, sharpness, decFactor)
+        self.assertEqual(fb1['numbands'], 33)
+        self.assertEqual(fb1['numhalfbands'], 64)
+        self.assertEqual(fb1['dfactor'], 16)
+        self.assertEqual(fb1['bandwidths'], 0.03125)
+        self.assertEqual(fb1['afilterorders'], 575)
+        self.assertEqual(fb1['sfilterorders'], 63)
+        self.assertEqual(fb1['fshift'], 1)
+        self.assertEqual(fb1['stft'], 1)
+        self.assertEqual(fb1['keeptransients'], 1)
+        # arrays
+        self.assertEqual(fb1['afilters'][0].size, 576)
+        self.assertEqual(fb1['sfilters'][0].size, 64)
+        a = np.array(fb1['afilters'][0])
+
     def test_designfilterbank(self):
         filtbankparams = designfilterbankstft(numhalfbands=9, sharpness=1, dfactor=1)
         self.assertEqual(filtbankparams['numbands'], 5)
@@ -49,72 +69,8 @@ class TestDesignFilterBankSTFT(unittest.TestCase):
         np.testing.assert_array_almost_equal(filterbankparams['centers'],
                                              [0, 0.2000, 0.4000, 0.6000, 0.8000, 1])
         self.assertEqual(filterbankparams['bandwidths'], 0.2000)
-        np.testing.assert_array_almost_equal(filterbankparams['afilters'][0],
-                                             [
-                                                 0.0016,
-                                                 0.0015,
-                                                 0.0014,
-                                                 0.0011,
-                                                 0.0005,
-                                                 - 0.0006,
-                                                 - 0.0021,
-                                                 - 0.0040,
-                                                 - 0.0062,
-                                                 - 0.0084,
-                                                 - 0.0102,
-                                                 - 0.0111,
-                                                 - 0.0105,
-                                                 - 0.0080,
-                                                 - 0.0033,
-                                                 0.0039,
-                                                 0.0134,
-                                                 0.0250,
-                                                 0.0380,
-                                                 0.0518,
-                                                 0.0654,
-                                                 0.0778,
-                                                 0.0881,
-                                                 0.0955,
-                                                 0.0994,
-                                                 0.0994,
-                                                 0.0955,
-                                                 0.0881,
-                                                 0.0778,
-                                                 0.0654,
-                                                 0.0518,
-                                                 0.0380,
-                                                 0.0250,
-                                                 0.0134,
-                                                 0.0039,
-                                                 - 0.0033,
-                                                 - 0.0080,
-                                                 - 0.0105,
-                                                 - 0.0111,
-                                                 - 0.0102,
-                                                 - 0.0084,
-                                                 - 0.0062,
-                                                 - 0.0040,
-                                                 - 0.0021,
-                                                 - 0.0006,
-                                                 0.0005,
-                                                 0.0011,
-                                                 0.0014,
-                                                 0.0015,
-                                                 0.0016,
-                                             ], decimal=3)
+        my_values = list(filterbankparams['afilters'][0])
         self.assertEqual(filterbankparams['afilterorders'], 49)
-        np.testing.assert_array_almost_equal(filterbankparams['sfilters'][0], [
-            -0.7481,
-            - 0.7353,
-            0.9549,
-            5.7474,
-            9.7941,
-            9.7941,
-            5.7474,
-            0.9549,
-            - 0.7353,
-            - 0.7481,
-        ], decimal=3)
         self.assertEqual(filterbankparams['sfilterorders'], 9)
         self.assertEqual(filterbankparams['fshift'], 1)
         self.assertEqual(filterbankparams['stft'], 1)
@@ -126,8 +82,51 @@ class TestDesignFilterBankSTFT(unittest.TestCase):
         result = windowBandwidth([.1, .2, 1, 1, 1, 1, .21, .1], .4)
         self.assertEqual(result, .75)
 
+    def test_win(self):
+        compWin = STFTwindow(np.array([.1, .2, .25, .5, .5, .5, .5, .25, .2, .1]), 2, 2)
+        np.testing.assert_array_almost_equal(compWin.T, [[0.816326530612245, 0.816326530612245]])
+        compWin = STFTwindow(np.array([.1, .2, .25, .5, .5, 1, 1, .5, .5, .25, .2, .1]), 2, 4)
+        np.testing.assert_array_almost_equal(compWin.T, [
+            [-0.429653364404311, 1.150597145353918, 1.150597145353918, -0.429653364404311]])
+        compWin = STFTwindow(np.array([.1, .2, .25, .5, .5, 1, 1, .5, .5, .25, .2, .1]), 1, 4)
+        np.testing.assert_array_almost_equal(compWin.T, [[-.6, .8, .8, -.6]])
+        compWin = STFTwindow(
+            np.array(
+                [.1, .1, .1, .1, .1, .1, .1, .1, .25, .25, .25, .25, .25, .25, .25, .25, .5, .5, .5, .5, .5, .5, .1, .1,
+                 .1, .1, .1, .1, .1, .1]), 8, 6)
+        np.testing.assert_array_almost_equal(compWin.T, [
+            [1.459854014598540, 1.459854014598540, 0.632911392405063, 0.632911392405063, 0.729927007299270,
+             0.729927007299270]])
+
+    def test_generate_analysis_win(self):
+        analysis_win = generate_analysis_win(8,2,8)
+        np.testing.assert_array_almost_equal(analysis_win, [
+            0.020725388601036,
+            0.065594479571239,
+            0.166414411818628,
+            0.247265720009097,
+            0.247265720009097,
+            0.166414411818628,
+            0.065594479571239,
+            0.020725388601036,
+        ])
+
     def test_downsampling(self):
         x = [1, 2, 3, 4, 5, 6, 7, 8]
         self.assertEqual(x, downsample(x, 1, 0))
         self.assertEqual([1, 3, 5, 7], downsample(x, 2, 0))
         self.assertEqual([2, 4, 6, 8], downsample(x, 2, 1))
+        x = [.001, .002, .01, .02, .1, .2, 1, 2]
+        value = downsample(x, 4, 0)
+        np.testing.assert_array_equal([.001, .1], value)
+        x = np.array([[.001, .002, .01, .02, .1, .2, 1, 2]])
+        value = downsample(x, 4, 0)
+        np.testing.assert_array_equal([[.001, .1]], value)
+        x = np.array([[.001, .002, .01, .02, .1, .2, 1, 2]]).T
+        value = downsample(x, 4, 0)
+        np.testing.assert_array_equal([[.001], [.1]], value)
+
+    def test_upsampling(self):
+        x = np.array([[.1, .2, 1, 2]])
+        value = upsample(x, 4, 0)
+        np.testing.assert_array_equal(value, [.1, 0, 0, 0, .2, 0, 0, 0, 1, 0, 0, 0, 2, 0, 0, 0])
