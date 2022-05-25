ad6ee0400363b7a7a8e752e04f584a794465281d
Miguel de Sousa <mfsousa@poli.ufrj.br>
2022-05-25 11:01:36 -0300

    fix indexes

--- modulation_toolbox_py/filterbank/designfilterbank_stft.py
+++ modulation_toolbox_py/filterbank/designfilterbank_stft.py
@@ -10,15 +10,13 @@ def designfilterbankstft(numhalfbands: int, sharpness: int = 1, dfactor: int = 1
     aOrder = winlen - 1
     errorCheck(numhalfbands, sharpness, dfactor, fshift)
     # design analysis window
-    n = np.arange((-winlen - 1) / 2, (winlen - 1) / 2, 1)
-    analysisWin = np.hamming(winlen)
-    analysisWin = analysisWin * scipy.special.diric((2 * np.pi / winlen * n), sharpness)
-    analysisWin = analysisWin / sum(analysisWin)
+    n = np.arange((-winlen + 1) / 2, (winlen + 1) / 2, 1)
+    analysisWin = generate_analysis_win(n, sharpness, winlen)
     synthWin = STFTwindow(analysisWin, dfactor, numhalfbands)
     sOrder = len(synthWin) - 1
     bandwidths = windowBandwidth(analysisWin, 1 / np.sqrt(2))
     numbands = int(np.floor(numhalfbands / 2) + 1)
-    centers = np.arange(0, ((2 * numbands)-1) / numhalfbands, 2 / numhalfbands)
+    centers = np.arange(0, ((2 * numbands) - 1) / numhalfbands, 2 / numhalfbands)
     filtbankparams = dict()
     filtbankparams['numbands'] = numbands
     filtbankparams['numhalfbands'] = numhalfbands
@@ -33,6 +31,13 @@ def designfilterbankstft(numhalfbands: int, sharpness: int = 1, dfactor: int = 1
     filtbankparams['stft'] = 1
     filtbankparams['keeptransients'] = keeptransients
     return filtbankparams
+
+
+def generate_analysis_win(n, sharpness, winlen):
+    analysisWin = np.hamming(winlen)
+    analysisWin = analysisWin * scipy.special.diric((2 * np.pi / winlen * n), sharpness)
+    analysisWin = analysisWin / sum(analysisWin)
+    return analysisWin
 
 
 def errorCheck(numhalfbands, sharpness, dfactor, fshift):
@@ -57,9 +62,8 @@ def STFTwindow(window, hopDistance, DFTsize):
     if M % 1 != 0 or M % 2 == 0:
         ValueError('window length must be divisible by DFT size with odd value')
 
-    F = np.zeros((int(M * min(R, L2)), L2))  # downsampled subsets of analysis window
+    F = np.zeros((int(M * min(R, L2)), L2))  # down-sampled subsets of analysis window
     D = np.zeros((int(M * min(R, L2)), 1))
-
     for p in range(int(M)):
         for n in range(min(int(R), int(L2))):
             temp = upsample(downsample(window[p * L2: (p + 1) * L2], R, n), R, n)
@@ -68,7 +72,7 @@ def STFTwindow(window, hopDistance, DFTsize):
             elif len(temp) > L2:
                 temp = temp[0:L2]
             F[int(p * min(R, L2) + n), :] = temp.T
-            D[int(p * min(R, L2) + n)-1, 0] = p == M2
+            D[int(p * min(R, L2) + n), 0] = p == M2
     compWin = pinv(F) @ D
     compWin = compWin[::-1]
     if transpose:
@@ -88,7 +92,8 @@ def windowBandwidth(window, magnitude):
 
 def upsampled_frame(sample, dfactor):
     frame = np.zeros(dfactor).tolist()
-    frame[0] = sample[0]
+    frame[0] = sample
+    # frame[0] = sample[0]
     return frame
 
 
@@ -97,12 +102,21 @@ def upsample(x: np.array, dfactor, phase: int = 0):
         raise ValueError('phase must be lower than dfactor')
     # if len(np.shape(x)) == 2:
     #     x = x.reshape(-1)
-    lists = [upsampled_frame(i, dfactor) for i in x]
+    if type(x) is np.ndarray and len(x.shape) == 2 and x.shape[0] == 1:
+        lists = [upsampled_frame(i, dfactor) for i in x[0, :]]
+    elif type(x) is np.ndarray and len(x.shape) == 2 and x.shape[1] == 1:
+        lists = [upsampled_frame(i, dfactor) for i in x[:, 0]]
+    else:
+        lists = [upsampled_frame(i, dfactor) for i in x]
     y = sum(lists, [])
     return np.roll(y, phase)
 
 
-def downsample(x: np.array, dfactor, phase: int = 0):
+def downsample(x, dfactor, phase: int = 0):
     if phase > dfactor:
         raise ValueError('phase must be lower than dfactor')
+    if type(x) is np.ndarray and len(x.shape) == 2 and x.shape[0] == 1:
+        return x[:, phase::dfactor]
+    if type(x) is np.ndarray and len(x.shape) == 2 and x.shape[1] == 1:
+        return x[phase::dfactor, :]
     return x[phase::dfactor]
