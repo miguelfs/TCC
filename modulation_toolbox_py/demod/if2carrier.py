bcd2185e4ae7808f5c75dc0b31828002ae02bcd7
Miguel de Sousa <mfsousa@poli.ufrj.br>
2023-03-30 16:02:40 -0300

    clear

--- modulation_toolbox_py/demod/if2carrier.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-
-def if2carrier(F):
-    fs = 2
-    return np.exp(2j * np.pi / fs * np.cumsum(np.array(F).T, axis=0)).T
--- modulation_toolbox_py/demod/modrecon.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-
-def modrecon(M: np.array, C: np.array) -> np.array:
-    if np.shape(M) != np.shape(C):
-        raise IndexError('M and C shapes must be equal')
-    return M * C
